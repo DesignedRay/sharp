88e5f7314d7b8d838301159174010a74b9184edd
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-05 20:51:35 +0300

    Equalization

---
+++ Program_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            for (int i = 0; i < arr.Length; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[0] == arr[1] || arr[1] == arr[2])
+            {
+                if (arr[0] == arr[2]) Console.WriteLine("3");
+                else Console.WriteLine("2");
+            }
+            else Console.WriteLine("0");
+            Console.ReadKey();
+        }
+    }
+}
