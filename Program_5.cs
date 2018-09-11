35c168dad0f9d90adf1a8094567bfc5be71e3a74
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-11 23:45:28 +0300

    Payday

---
+++ Program_5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int payWork = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("В месяц : {0} руб, в день {1} руб, в час {2} руб, в минуту {3} руб", payWork , payWork / 30 , payWork / 720 , payWork / 43200);
+        }
+    }
+}
