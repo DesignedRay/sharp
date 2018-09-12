8972addf33e48ba938fca02a3e879331a42ba8c9
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-12 23:08:19 +0300

    Calculator

---
+++ Program_8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int x1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Аргументов 1/2");
+                int x2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Аргументов 2/2");
+                String userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "+":
+                        Console.WriteLine(x1 + x2);
+                        break;
+                    case "-":
+                        Console.WriteLine(x1 - x2);
+                        break;
+                    case "*":
+                        Console.WriteLine(x1 * x2);
+                        break;
+                    case "/":
+                        Console.WriteLine(x1 / x2);
+                        break;
+            }
+        }
+    }
+}
