622e3df65b4a5a8ee8f5e56b357402d71973cd6e
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-12 22:25:53 +0300

    Check password (While)

---
+++ Program_7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const String pass = "1234";
+            while (true)
+            {
+                if (Console.ReadLine() == pass) {
+                    Console.WriteLine(1);
+                    break;
+                }
+                else Console.WriteLine(0);
+            }
+        }
+    }
+}
