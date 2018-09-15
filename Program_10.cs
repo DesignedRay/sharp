41bf5441db971daac85f02dd4e5c20218953ed30
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-15 19:53:33 +0300

    LuckyTicket

---
+++ Program_10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ticketNumber = Console.ReadLine();
+            int sumLeft = int.Parse(ticketNumber[0].ToString()) + int.Parse(ticketNumber[1].ToString()) + int.Parse(ticketNumber[2].ToString());
+            int sumRight = int.Parse(ticketNumber[3].ToString()) + int.Parse(ticketNumber[4].ToString()) + int.Parse(ticketNumber[5].ToString());
+            Console.WriteLine(sumLeft - sumRight);
+        }
+    }
+}
