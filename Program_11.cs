6ef20602eba4da713f87c5203f43991d533a59b2
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-17 21:27:39 +0300

    Take Gift Code in File and check

---
+++ Program_11.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activaitionKey = File.ReadAllText("key.txt");
+            string[] keysBrut = File.ReadAllLines("brut.txt");
+            for (int i = 0; i < keysBrut.Length ; i++)
+            {
+                Console.WriteLine("Attempt {0} ...",i+1);
+                if (activaitionKey == keysBrut[i])
+                {
+                    Console.WriteLine(" {0}  - подтвержден , программа завершила работу.", keysBrut[i]);
+                    break;
+                }
+                else Console.WriteLine(" {0}  - не подтвержден", keysBrut[i]);
+            }
+        }
+    }
+}
