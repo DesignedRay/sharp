69b3695de84ddab28600e9e3d98b199d20ee7857
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-20 23:37:37 +0300

    Lotery

---
+++ Program14.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double year = double.Parse(Console.ReadLine());
+            year = ((((((year * 2 + 5) * 4 / 20) + 3) * 5) - 20) / year) + 5;
+            Console.WriteLine((year == 7) ? "YES" : "NO");
+        }
+    }
+}
