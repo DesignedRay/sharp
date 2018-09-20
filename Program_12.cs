cdb6be229a451d2908058e18219430046d8bb2bc
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-20 16:27:30 +0300

    Eng char

---
+++ Program_12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] en_al = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            char userInput = char.Parse(Console.ReadLine());
+            for (int i = 0; i < en_al.Length; i++)
+            {
+                if (en_al[i] == userInput)
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+            }
+        }
+    }
+}
