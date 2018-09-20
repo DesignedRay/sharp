b66d6f2a7ee2b0abe5023cf8c0f0c7010125b47b
Designed <stas-andreev2024.andreev@yandex.ru>
2018-09-20 17:09:48 +0300

    cube

---
+++ Program_13.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "", y = "";
+            string userInput = Console.ReadLine();
+            for (int i = 0,j=99; i < userInput.Length; i++)
+            {
+                if (!(userInput[i] == 'd'))
+                {
+                    if (i < j)
+                    {
+                        x += Convert.ToString(userInput[i]);
+                    }
+                    else if (i > j)
+                    {
+                        y += Convert.ToString(userInput[i]);
+                    }
+                }
+                else
+                {
+                    j = i;
+                }
+            }
+            Console.WriteLine("{0}-{1}",x,int.Parse(x)*int.Parse(y));
+        }
+    }
+}
